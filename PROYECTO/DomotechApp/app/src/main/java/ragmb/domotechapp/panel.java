c74151fe03b4a07fa71f7e1fed11ad0d678ecd3a
gtramos10 <ing.germanramosleon@gmail.com>
2015-10-09 01:13:29 -0500

    Parte 2 Proyecto

--- PROYECTO/DomotechApp/app/src/main/java/ragmb/domotechapp/panel.java
+++ PROYECTO/DomotechApp/app/src/main/java/ragmb/domotechapp/panel.java
@@ -1,13 +1,29 @@
 package ragmb.domotechapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class panel extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel);
+
+        btn1 = (Button)findViewById(R.id.btnexit);
+
+        btn1 .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+
+            }
+        });
+
     }
+
 }
